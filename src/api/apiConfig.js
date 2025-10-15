c7f2833b0bfe4b58b6ea0031de2df15826f569c4
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2025-10-15 12:02:43 -0300

    Fix: .env file

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,9 +1,13 @@
+const baseUrl = import.meta.env.VITE_TMDB_BASE_URL;
+const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+const keywordUrl = import.meta.env.VITE_TMDB_KEYWORD_URL;
+
 const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '241b542b668244dd2ea182030824f211',
-    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
-    keyword: 'https://api.themoviedb.org/3/discover/movie?api_key=5c0f12389eb757b9cbf2318762035360&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&with_keywords=289844&with_watch_monetization_types=free'
-}
+  baseUrl,
+  apiKey,
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
+  keyword: `${keywordUrl}&api_key=${apiKey}`,
+};
 
 export default apiConfig;
